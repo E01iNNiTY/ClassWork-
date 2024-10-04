2c47b2313a542af8eaea5d619afcfeef986aa5f7
E01iNNiTY <blizzard2521@gmail.com>
2024-10-04 11:23:45 -0400

    DLC

---
+++ output.py
@@ -0,0 +1,5 @@
+print("Aventura Hermanita")
+print("Aventura El Malo")
+print("Aventura Los Infieles")
+
+# Output will be what prints out to the terimnal. What is entered into the program is output back to the terminal 
