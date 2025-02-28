b0758605bb81d3a4ff59fde7235f9c283460ee9e
E01iNNiTY <blizzard2521@gmail.com>
2025-02-28 11:22:45 -0500

    "Debug"

---
+++ Debug.py
@@ -0,0 +1,16 @@
+def main():
+    for i in range(41):  
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+
+
+if __name__ == "__main__":
+    main()
